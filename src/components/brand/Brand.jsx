8bad2fd16362f7fbf8083e3304201c84de2dbe0f
Whitney <whitneydawson123@gmail.com>
2023-06-08 08:19:57 -0400

    fixed eslint issue

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -9,16 +9,16 @@ const Brand = () => {
         <img src={google} alt="google" />
       </div>
       <div>
-        <img src={slack} />
+        <img src={slack} alt="slack"/>
       </div>
       <div>
-        <img src={atlassian} />
+        <img src={atlassian} alt="atlassian" />
       </div>
       <div>
-        <img src={dropbox} />
+        <img src={dropbox} alt="dropbox" />
       </div>
       <div>
-        <img src={shopify} />
+        <img src={shopify} alt="shopify" />
       </div>
     </div>
   );
